2e8f4f236b9f05196660f430ede0f39b39f7ab03
Cheezegami <superronald@live.nl>
2017-11-20 02:23:43 +0100

    foundation

--- objects/player.gd
+++ objects/player.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
 const speed = 200
+const shootDelay = 0.3
 
+var shootTimer = 0
 var velocity = Vector2()
 var shuriken = preload("res://objects/shuriken.xml");
 var shurikenCount = 0;
@@ -9,6 +11,8 @@ func _ready():
     set_fixed_process(true)
 
 func _fixed_process(delta):
+    if(shootTimer > 0):
+        shootTimer-= delta
     var shurikenScript;
     velocity = Vector2(0,0)
 
@@ -23,32 +27,34 @@ func _fixed_process(delta):
     var leftShootButton = Input.is_action_pressed("ui_shoot_left")
 
     if(leftButton):
-        velocity.x = -1
+        velocity.x += -1
     elif(rightButton):
-        velocity.x = 1
+        velocity.x += 1
     if(upButton):
-        velocity.y = -1
+        velocity.y += -1
     elif(downButton):
-        velocity.y = 1
+        velocity.y += 1
 
-    if(leftShootButton || rightShootButton || upShootButton || downShootButton):
-        shurikenCount += 1;
+    if((leftShootButton || rightShootButton || upShootButton || downShootButton) && shootTimer <= 0):
+        shurikenCount += 1
         var shuriken_instance = shuriken.instance()
         shuriken_instance.set_name("Shuriken"+str(shurikenCount))
-        add_child(shuriken_instance)
-        shuriken_instance.set_owner(self)
+        get_parent().add_child(shuriken_instance)
         #use below variable for individual node access.
-        var shuriken_instance_node = get_node("Shuriken"+str(shurikenCount))
+        var shuriken_instance_node = get_parent().get_node("Shuriken"+str(shurikenCount))
+        shuriken_instance_node.add_collision_exception_with(self)
+        shuriken_instance_node.set_pos(self.get_global_pos())
         if(leftShootButton):
-            shuriken_instance_node.projVelocity.x = -1
+            shuriken_instance_node.projVelocity += Vector2(-1,0) + (velocity*0.5)
         elif(rightShootButton):
-            shuriken_instance_node.projVelocity.x = 1
+            shuriken_instance_node.projVelocity += Vector2(1,0) + (velocity*0.5)
         if(upShootButton):
-            shuriken_instance_node.projVelocity.y = -1;
+            shuriken_instance_node.projVelocity += Vector2(0,-1) + (velocity*0.5)
         elif(downShootButton):
-            shuriken_instance_node.projVelocity.y = 1;        
+            shuriken_instance_node.projVelocity += Vector2(0,1) + (velocity*0.5)
+        shootTimer = shootDelay      
 
-    var motion = velocity.normalized() * speed * delta
+    var motion = velocity * speed * delta
     move(motion)
     if (is_colliding()):
         var n = get_collision_normal()
--- objects/shuriken.gd
+++ objects/shuriken.gd
@@ -1,17 +1,33 @@
 extends KinematicBody2D
 
-const speed = 200
+var speed = 200
+const lifeTime = 10
+
+var timeLeft
 var projVelocity = Vector2()
 
 func _ready():
+    timeLeft = lifeTime
     set_fixed_process(true)
     projVelocity = Vector2(0,0)
 
 func _fixed_process(delta):
-    var motion = projVelocity.normalized() * speed * delta
+    timeLeft -= delta
+    speed *= 0.995
+    var motion = projVelocity * speed * delta
     move(motion)
     if (is_colliding()):
         var n = get_collision_normal()
         motion = n.slide(motion)
-        projVelocity = n.slide(projVelocity)
+        projVelocity = n.reflect(projVelocity)
         move(motion)
+    if(timeLeft+0.25 < lifeTime):
+        remove_collision_exception_with(get_parent().get_node("Player"))
+    if(timeLeft <= 0):
+        queue_free()
+        pass
+    if(motion == Vector2(0,0)):
+        timeLeft-= delta * 10
+
+func _on_VisibilityNotifier2D_exit_screen():
+	queue_free()
